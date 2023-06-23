c21e00e179d8e7787e0f47ff25045b4e74debe81
snehamoybag <snehamoybag2@gmail.com>
2023-06-23 14:13:58 +0530

    fix some bugs

--- src/js/index.js
+++ src/js/index.js
@@ -1,27 +1,25 @@
 // stylesheet
-import '/scss/style.scss';
+import "/scss/style.scss";
 // AOS Library
-import AOS from 'aos';
-import 'aos/dist/aos.css'; // You can also use <link> for styles
+import AOS from "aos";
+import "aos/dist/aos.css"; // You can also use <link> for styles
 AOS.init();
 
 // remove animation caused horizontal scrollbar
 const animationFadeLeft = document.querySelectorAll('[data-aos="fade-left"]');
 const animationFadeRight = document.querySelectorAll('[data-aos="fade-right"]');
 
-const overFlowXAnimationEls = [
-  animationFadeLeft,
-  animationFadeRight,
-];
+const overFlowXAnimationEls = [animationFadeLeft, animationFadeRight];
 
-function removeHorizontalScrollBar(elArr){
+function removeHorizontalScrollBar(elArr) {
   // iterate animations array
   for (let i = 0; i < elArr.length; i++) {
     // iterate all the elements as 'querySelectorAll' returns a node list
     for (let j = 0; j < elArr[i].length; j++) {
-      elArr[i][j].parentElement.style.overflowX = 'hidden';
+      elArr[i][j].parentElement.style.overflowX = "hidden";
     }
   }
 }
 // remove horizontal scroll bar
 removeHorizontalScrollBar(overFlowXAnimationEls);
+
